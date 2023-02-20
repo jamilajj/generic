f4c7ef5851bd8264076ec68bd21a63093ce3c024
unknown <jamilajj>
2023-02-20 23:50:39 +0400

    generic

---
+++ Generic/MyList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generic
+{
+    internal class MyList<T> 
+    {
+        public T[] Arr = new T[0];
+        public void Add (T value)
+        {
+            Array.Resize(ref Arr, Arr.Length + 1);
+            Arr[Arr.Length - 1] = value;
+        }
+        public void Remove(int index)
+        {
+            if (index < 0 || index >= Arr.Length)
+                throw new IndexOutOfRangeException();
+            for (int i = index; i < Arr.Length - 1; i++)
+            {
+                var temp = Arr[i];
+                Arr[i] = Arr[i + 1];
+                Arr[i + 1] = temp;
+            }
+            Array.Resize(ref Arr, Arr.Length - 1);
+        }
+    }
+}
---
+++ Generic/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Generic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          ArrayList arrayList= new ArrayList();
+            arrayList.Add(20);
+            arrayList.Add(30);
+            arrayList.Add(40);
+            arrayList.Add("50A");
+
+            //  arrayList.Remove(30);
+            ///   arrayList.RemoveAt(2);
+            //arrayList.Insert(arrayList.Count, arrayList);
+            foreach(var item in arrayList)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("==========qoue========");
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(20);
+            queue.Enqueue(30);
+            queue.Enqueue(50);
+            
+           // var value=queue.Dequeue();
+          //  value = queue.Peek();
+         //   value=queue.Dequeue();
+
+            foreach (var item in queue) 
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("=================stack=======");
+            Stack<int> stack = new Stack<int>();
+            stack.Push(20);
+            stack.Push(30);
+            stack.Push(50);
+            
+            var value= stack.Pop();
+            foreach (var item in stack)
+            {
+                Console.WriteLine($"{item}");
+            }
+            arrayList.AddRange(stack);
+            Console.WriteLine("Arraylist");
+            foreach (var item in arrayList)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("==========sortedlist=======");
+           SortedList sortedList= new SortedList();
+            sortedList.Add("ali", 1);
+            sortedList.Add("veli", 2);
+            sortedList.Add("totu", 3);
+            Console.WriteLine(sortedList["mli"]);
+            throw new FormatException();
+
+        }
+    }
+}
